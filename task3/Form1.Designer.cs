273d2c087eac8831eefba90530e42706baea0bd2
Je_day <je.mouth@gmail.com>
2018-10-10 23:25:09 +0300

    curves done

--- task3/Form1.Designer.cs
+++ task3/Form1.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(664, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(683, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(95, 120);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Points";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Point 0";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Point 1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(58, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Point 2";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 89);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(58, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Point 3";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Bézier curve";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- task3/Form1.cs
+++ task3/Form1.cs
@@ -12,9 +12,81 @@ namespace task3
 {
     public partial class Form1 : Form
     {
+        private PointF p0,p1,p2,p3;
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            var font = new Font("Tahoma", 12, FontStyle.Bold);
+            var br = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            Pen blackPen = new Pen(Color.Black, 1);
+            blackPen.DashPattern =new float[] { 1,1 };
+
+            e.Graphics.DrawLines(blackPen, new PointF[] { p0, p1, p2, p3 });
+            e.Graphics.DrawEllipse(new Pen(Color.Black), p0.X - (float)1.5, p0.Y - (float)1.5, 3, 3);
+            e.Graphics.DrawEllipse(new Pen(Color.Black), p1.X - (float)1.5, p1.Y - (float)1.5, 3, 3);
+            e.Graphics.DrawEllipse(new Pen(Color.Black), p2.X - (float)1.5, p2.Y - (float)1.5, 3, 3);
+            e.Graphics.DrawEllipse(new Pen(Color.Black), p3.X - (float)1.5, p3.Y - (float)1.5, 3, 3);
+            e.Graphics.DrawString("P0", font, br, p0);
+            e.Graphics.DrawString("P1", font, br, p1);
+            e.Graphics.DrawString("P2", font, br, p2);
+            e.Graphics.DrawString("P3", font, br, p3);
+           
+
+            List<PointF> l = new List<PointF>();
+            for (float t = 0; t <= 1; t += (float)0.01) {
+                l.Add(calculate_point(t));
+                //e.Graphics.DrawEllipse(new Pen(Color.Red), p.X - (float)1.5, p.Y - (float)1.5, 3, 3);
+
+            }
+            e.Graphics.DrawLines(new Pen(Color.Aquamarine,2), l.ToArray());
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked) {
+                p0 = e.Location;
+            }
+            else if (radioButton2.Checked) {
+                p1 = e.Location;
+            }
+
+            else if (radioButton3.Checked) {
+                p2 = e.Location;
+            }
+
+            else if (radioButton4.Checked) {
+                p3 = e.Location;
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private PointF calculate_point(float t) {
+            float mt = 1 - t;
+            float mt2 = mt*mt;
+            float x;
+            float y;
+            float t2 = t*t;
+            x = p3.X*t2*t;
+            y = p3.Y*t2*t;
+            x += 3 * t2 * p2.X * mt;
+            y += 3 * t2 * p2.Y * mt;
+            x += 3 * t * p1.X * mt2;
+            y += 3 * t * p1.Y * mt2;
+            x += p0.X * mt2*mt;
+            y += p0.Y * mt2*mt;
+            return new PointF(x, y);
+
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            p0 = new PointF(100, 100);
+            p1 = new PointF(100, 200);
+            p2 = new PointF(200, 200);
+            p3 = new PointF(200 ,100);
+
         }
     }
 }
